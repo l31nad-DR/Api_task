f97019a381ed55a54143d048f77a53a2f1296749
lonewolf-DR <119358820+lonewolf-DR@users.noreply.github.com>
2023-08-14 19:09:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-sample_list = [1, 1, 8, 4, 4, 2, 4, 8]
-sample_dict = {}
 
-for i in sample_list:
-    if i in sample_dict:
-        sample_dict[i] += 1
-    else:
-        sample_dict[i] = 1
-
-print(sample_dict)
